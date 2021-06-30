12cbf1754e9b9c7faabc66d8dc975d974d3564e7
AcidAssassinator <44037319+AcidAssassinator@users.noreply.github.com>
2021-06-30 15:07:21 -0500

    Update Blackjack.java

--- src/main/java/Blackjack.java
+++ src/main/java/Blackjack.java
@@ -1,6 +1,9 @@
 import gameplay.Table;
 
 public class Blackjack {
+    /*
+    * Yes I know this is dumb. I don't really care though.
+    */
     public static void main(String[] args) {
         Table.clearConsole();
         new Table();
